9860e41355ace846f0dcabba6363de34131e7b88
Mrs. Latimer <46603910+klatimer1@users.noreply.github.com>
2022-10-17 13:27:28 -0500

    Add files via upload

---
+++ SwitchDemoMonth.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.switchdemo;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author klatimer
+ */
+public class SwitchDemo {
+
+    public static void main(String[] args) {
+        String monthInput = JOptionPane.showInputDialog("Please enter the month in numeric form");
+        int month = Integer.parseInt(monthInput);
+        String monthString;
+        switch (month) {
+            case 1:  monthString = "January";
+                     break;
+            case 2:  monthString = "February";
+                     break;
+            case 3:  monthString = "March";
+                     break;
+            case 4:  monthString = "April";
+                     break;
+            case 5:  monthString = "May";
+                     break;
+            case 6:  monthString = "June";
+                     break;
+            case 7:  monthString = "July";
+                     break;
+            case 8:  monthString = "August";
+                     break;
+            case 9:  monthString = "September";
+                     break;
+            case 10: monthString = "October";
+                     break;
+            case 11: monthString = "November";
+                     break;
+            case 12: monthString = "December";
+                     break;
+            default: monthString = "Invalid month";
+                     break;
+        }
+        //System.out.println(monthString);
+        JOptionPane.showMessageDialog(null,"The month you entered is " +  monthString);
+    }
+    }
+
